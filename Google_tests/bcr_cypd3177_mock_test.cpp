99c2407e7dbaf75a026bba6fc309c80a31a7f048
leosun <leosunsl@outlook.com>
2022-03-25 10:59:38 +0800

    driving bcr mocking

--- Google_tests/bcr_cypd3177_mock_test.cpp
+++ Google_tests/bcr_cypd3177_mock_test.cpp
@@ -13,16 +13,29 @@ class BCR_Cypd3177_Mock_Test:public testing::Test
 protected:
     virtual void SetUp()
     {
-        std::cout<<"SetUp"<<std::endl;
+//        std::cout<<"SetUp"<<std::endl;
     }
     virtual void TearDown()
     {
-        std::cout<<"TearDown"<<std::endl;
+//        std::cout<<"TearDown"<<std::endl;
     }
 };
 
 TEST_F(BCR_Cypd3177_Mock_Test, ReturnNegativeOneWhenInputIsNull) {
-    uint8_t input[4] = {0};
-    EXPECT_EQ(platform_write(NULL, 0, input, 0), -1);
     EXPECT_EQ(platform_write(NULL, 0, NULL, 1), -1);
+}
+TEST_F(BCR_Cypd3177_Mock_Test, ReturnNegativeOneWhenInputLengthIsZero) {
+    uint8_t input[4] = {0};
+    EXPECT_EQ(platform_write(input, 0, input, 0), -1);
+}
+TEST_F(BCR_Cypd3177_Mock_Test, Return0WhenInputIsNotEmptyNotNone) {
+    uint8_t input[4] = {0};
+    EXPECT_EQ(platform_write(NULL, 0, input, sizeof(input)), 0);
+}
+TEST_F(BCR_Cypd3177_Mock_Test, ReadReturnNegativeOneWhenOutBufferIsNull) {
+    EXPECT_EQ(platform_read(NULL, 0, NULL, 1), -1);
+}
+TEST_F(BCR_Cypd3177_Mock_Test, ReadReturnNegativeOneWhenOutBufferLengthIsZero) {
+    uint8_t input[4] = {0};
+    EXPECT_EQ(platform_read(input, 0, input, 0), -1);
 }
