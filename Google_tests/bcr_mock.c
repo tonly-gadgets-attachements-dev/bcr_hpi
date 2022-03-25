73a7e3a4b309e230280852cd000673424d78a1c0
leosun <leosunsl@outlook.com>
2022-03-25 12:03:03 +0800

    add gmock assertions

--- Google_tests/bcr_mock.c
+++ Google_tests/bcr_mock.c
@@ -4,8 +4,14 @@
 
 #include "bcr_mock.h"
 
-int32_t platform_write(void *handle, uint16_t reg, uint8_t *bufp, uint16_t len)
+int32_t platform_write(void *handle, uint16_t reg, const uint8_t *bufp, uint16_t len)
 {
+    if (bufp == NULL || len == 0) {
+        return -1;
+    }
+    return 0;
+}
+int32_t platform_read(void *handle, uint16_t reg, uint8_t *bufp, uint16_t len) {
     if (bufp == NULL || len == 0) {
         return -1;
     }
